e103a019d472303ab4a4969ffd0f5508fba44430
cclloyd <cclloyd@cclloyd.com>
2022-04-13 12:03:17 -0400

    Updated CI

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-import os
 from distutils.core import setup
-import pypandoc
-
-
-def read_file(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
-        return file.read()
-
-
-try:
-    description = pypandoc.convert_file('README.md', 'rst')
-except (IOError, ImportError):
-    description = open('README.md').read()
 
 
 setup(
@@ -21,7 +8,6 @@ setup(
     license='MIT',
     description='Convert media files to a modern smaller equivalent.',
     long_description='For more information on how to use, visit the repo at https://gitlab.com/cclloyd1/webwatcher',
-    #long_description_content_type='text/markdown',
     author='cclloyd',
     author_email='cclloyd@cclloyd.com',
     url='https://gitlab.com/cclloyd1',
@@ -34,7 +20,7 @@ setup(
     python_requires='>=3.8',
     classifiers=[
         # "3 - Alpha"    "4 - Beta"    "5 - Production/Stable"
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Information Technology',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion',
@@ -47,4 +33,5 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
     ],
+    setup_requires=['wheel'],
 )
